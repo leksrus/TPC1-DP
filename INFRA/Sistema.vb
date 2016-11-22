7624c538784e7ae79b1d8dec27eb5bf5f08563a6
Aleksey Kotylev <aleks_rus@hotmail.com>
2016-11-22 17:32:18 -0300

    1.0

--- INFRA/Sistema.vb
+++ INFRA/Sistema.vb
@@ -39,13 +39,4 @@ Public Class Sistema
         End Set
     End Property
 
-    Public Shared Function ConcatString(cadena() As String) As String
-        Dim sb As New StringBuilder
-        Dim encriptador As New CryptoManager
-        For i = 0 To cadena.Length - 1
-            sb.Append(cadena(i))
-        Next
-        Return encriptador.ConvertToHash(sb.ToString)
-    End Function
-
 End Class
